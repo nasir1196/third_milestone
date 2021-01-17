ea1bcf8d7037394db6ef40332838185dcfe7edd6
Nasir Uddin <monforingnasir@gmail.com>
2021-01-17 11:57:47 +0600

    added file

---
+++ parseint_parsefloat.js
@@ -0,0 +1,46 @@
+var num1 = 25;
+var num2 = 25.8;
+console.log(num1 + num2);
+
+var num1= 25;
+var num2 ="25.8";
+num2 = parseInt(num2);
+console.log(num1+num2);
+
+var num1= 25;
+var num2 ="25.8";
+num2 =parseFloat(num2);
+console.log(num1+num2);
+
+var num1= 25;
+var num2 ="25.8";
+num2=+num2;
+console.log(num1+num2);
+
+var num1= 25;
+var num2 =25.8;
+num2=Math.ceil(num2);
+console.log(num1+num2);
+
+var num1= 25;
+var num2 =25.8;
+num2=Math.floor(num2);
+console.log(num1+num2);
+
+var num1= 25;
+var num2 =25.8;
+num2=Math.round(num2);
+console.log(num1+num2);
+
+var num1= 25;
+var num2 =25.49;
+num2=Math.round(num2);
+console.log(num1+num2);
+
+var absNumber= -6;
+var result = Math.abs(absNumber);
+console.log(result);
+
+var absNumber= 6;
+var result = Math.abs(absNumber);
+console.log(result);
