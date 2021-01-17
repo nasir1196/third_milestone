3850eeaa419a74d39bc16ceda8e89ea0a3d5252b
Nasir Uddin <monforingnasir@gmail.com>
2021-01-17 11:21:25 +0600

    added file

---
+++ index_of_split_change_case.js
@@ -0,0 +1,8 @@
+var promise = "I promise I will work hard BECOME a programmer";
+console.log(promise.indexOf("hard"));
+console.log(promise.indexOf("Hard")); //When the word can't find out from the string out put show -1
+console.log(promise.toUpperCase());
+console.log(promise);
+console.log(promise.toLocaleLowerCase());
+// console.log(promise.split("I"));
+console.log(promise.split(" "));
