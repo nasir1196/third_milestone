71619aa6ec0fc42036dfd4a2d1ef2f684b4a978e
Nasir Uddin <monforingnasir@gmail.com>
2021-01-17 11:00:10 +0600

    added variable type

---
+++ all_variable_type.js
@@ -0,0 +1,51 @@
+//integer type variable
+var number1 = 12;
+var number2 = 34;
+var result = number1 + number2;
+console.log(result);
+
+//float type variable 
+var float1 = 24.55;
+var float2 = 30.25;
+var floatResult = float1 + float2;
+console.log(floatResult);
+
+//string type variable 
+var firstName = "nasir";
+var lastName = " uddin";
+var fullName = firstName + lastName;
+console.log(fullName);
+
+//Boolean type variable 
+var hotDay = false;
+var coldDay = true;
+console.log(coldDay);
+console.log(hotDay);
+
+//integer type variable see typeof(function)
+var number1 = 12;
+var number2 = 34;
+var result = number1 + number2;
+console.log(typeof (result));
+
+
+//string type variable see typeof(function)
+var firstName = "nasir";
+var lastName = " uddin";
+var fullName = firstName + lastName;
+console.log(typeof (fullName));
+
+//Boolean type variable see typeof(function)
+var hotDay = false;
+var coldDay = true;
+console.log(typeof (coldDay));
+console.log(typeof (hotDay));
+
+//float type variable see typeof(function)
+var float1 = 24.55;
+var float2 = 30.25;
+var floatResult = float1 + float2;
+console.log(typeof (floatResult));
+
+var floating=25.55;
+console.log(typeof(floating));
--- declare_variable.js
+++ declare_variable.js
@@ -2,3 +2,9 @@ var f_name = "Nasir";
 var l_name = " Uddin";
 var full_name = f_name + l_name;
 console.log(full_name);
+
+var firstName ="nasir";
+var lastName ="Uddin";
+var fullName=firstName+lastName;
+console.log(fullName);
+
