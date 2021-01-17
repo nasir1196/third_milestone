4df598174ebde0bedf8854642040f4a1c5d2262f
Nasir Uddin <monforingnasir@gmail.com>
2021-01-17 13:22:48 +0600

    advance array

--- module-13/array.js
+++ module-13/array.js
@@ -1,4 +1,15 @@
 var friendsAge=[26,25,24,28,29];
 var rubel=friendsAge.indexOf(25);
 friendsAge[1]=27;
+// console.log(friendsAge);
+
+friendsAge.push(33);
+friendsAge.push(38);
+console.log(friendsAge.length);
+friendsAge.pop();
+friendsAge.unshift(38);
+friendsAge.unshift(40);
+friendsAge.shift();
+friendsAge.sort();
+friendsAge.reverse();
 console.log(friendsAge);
