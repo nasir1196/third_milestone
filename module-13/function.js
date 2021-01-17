2e6ccf2d7befc170fa75127a8a2e711cea016c34
Nasir Uddin <monforingnasir@gmail.com>
2021-01-17 16:05:46 +0600

    add function

---
+++ module-13/function.js
@@ -0,0 +1,11 @@
+function justCallMe(){
+    var love=10;
+    if(love==100){
+        console.log("Love is blind");
+    }
+    else{
+        console.log("Love isn't blind");
+    }
+    
+}
+justCallMe();
---
+++ module-13/function_parameter.js
@@ -0,0 +1,9 @@
+//functions multiple stepping using parameter;
+function add(num1, num2, num3) {
+    var result = num1 + num2 / num3;
+    return result;
+}
+var sum = add(40, 35, 3);
+console.log(sum);
+
+console.log(35/3);
