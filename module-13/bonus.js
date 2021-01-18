9afd288f912b0211ff2ca306223a661c59f53356
Nasir Uddin <monforingnasir@gmail.com>
2021-01-18 14:07:58 +0600

    added function vs for loop

--- module-13/bonus.js
+++ module-13/bonus.js
@@ -1,15 +1,29 @@
-// var marks = [23,54,32,63,26,95,42,35,29];
-// for (let i = 0; i < marks.length; i++) {
-//     var element = marks[i];
-//     console.log(element);  
-// }
+var marks = [23, 54, 32, 63, 26, 95, 42, 35, 29];
+for (let i = 0; i < marks.length; i++) {
+    var element = marks[i];
+    console.log(element);
+}
 
 let shop = [];
-var j=0;
+var j = 0;
 for (let i = 1; i <= 20; i++) {
-    shop.push('anythings'+i);
+    shop.push('anythings' + i);
     j++;
 }
 console.log(shop);
-console.log('operation',j);
+console.log('operation', j);
 
+function largestNumber(numbers) {
+    var larger = numbers[0];
+    for (var i = 0; i < numbers.length; i++) {
+        var element = numbers[i];
+        if (element > larger) {
+            larger = element;
+        }
+    }
+    return larger;
+}
+var numbers=[23, 54, 32, 63, 26, 95, 42, 35, 29];
+numbers.push(102,203,307,499);
+var output=largestNumber(numbers);
+console.log("largest Number ",output);
