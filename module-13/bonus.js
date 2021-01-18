773bf6ddc398ea5d38d6b7562c31fe4fe0d0e571
Nasir Uddin <monforingnasir@gmail.com>
2021-01-18 13:43:52 +0600

    added bonus js file

---
+++ module-13/bonus.js
@@ -0,0 +1,15 @@
+// var marks = [23,54,32,63,26,95,42,35,29];
+// for (let i = 0; i < marks.length; i++) {
+//     var element = marks[i];
+//     console.log(element);  
+// }
+
+let shop = [];
+var j=0;
+for (let i = 1; i <= 20; i++) {
+    shop.push('anythings'+i);
+    j++;
+}
+console.log(shop);
+console.log('operation',j);
+
