a3575b5a2e1698621a7c1e1f022d07be03485f00
Nasir Uddin <monforingnasir@gmail.com>
2021-01-19 21:22:28 +0600

    add prime number

---
+++ mudule-14/checkPrimeNumber.js
@@ -0,0 +1,23 @@
+function isPrime(n) {
+  for (let i = 2; i <= n - 1; i++) {
+    if (n % i == 0) {
+      return "This Number is not a Prime Number";
+    }
+  }
+  return "This Number is a Prime Number";
+}
+
+let result = isPrime(7);
+console.log(result);
+
+function checkPrime(n) {
+  for (let i = 2; i < n; i++) {
+    if (n % i == 0) {
+      return "This Number is not a Prime Number";
+    }
+  }
+  return "This Number is a Prime Number";
+}
+
+let checkResult = checkPrime(4);
+console.log(checkResult);
