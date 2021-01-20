7cbbbee7929eae1f0d8656b924ca792aa262d0b9
Nasir Uddin <monforingnasir@gmail.com>
2021-01-20 15:04:08 +0600

    added duplicate function

--- javaScript_problem_solving/duplicate.js
+++ javaScript_problem_solving/duplicate.js
@@ -8,3 +8,17 @@ for (let i = 0; i < numberList.length; i++) {
   }
 }
 console.log(freshList);
+
+function getDuplicate(list) {
+  let newList = [];
+  for (let i = 0; i < list.length; i++) {
+    let element = list[i];
+    let index = newList.indexOf(element);
+    if (index == -1) {
+      newList.push(element);
+    }
+  }
+  return newList;
+}
+let result = getDuplicate([2, 3, 4, 3, 4, 32, 54, 3, 2, 3, 2, 3, 4, 5, 6, 6]);
+console.log(result);
