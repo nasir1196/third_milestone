ea588ad0753a01e3ac4220b75bf7d353c1052ef4
Nasir Uddin <monforingnasir@gmail.com>
2021-01-21 18:43:08 +0600

    change fibo method

--- mudule-14/fibonacciSeriesRecursive.js
+++ mudule-14/fibonacciSeriesRecursive.js
@@ -1,8 +1,6 @@
 function fibonacci(n) {
-  if (n == 0) {
-    return [0];
-  } else if (n == 1) {
-    return [0, 1];
+   if (n == 1) {
+    return [0,1];
   } else {
     let fibo = fibonacci(n - 1);
     let nextElement = fibo[n - 1] + fibo[n - 2];
@@ -11,5 +9,5 @@ function fibonacci(n) {
   }
 }
 
-let result = fibonacci(50);
+let result = fibonacci(10);
 console.log(result);
